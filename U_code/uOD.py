6e091d6be7cc93c9829610065b8c8a24a8830627
erxiaozhou <651327272@qq.com>
2020-10-08 20:57:30 +0800

    daily update

--- U_code/uOD.py
+++ U_code/uOD.py
@@ -4,7 +4,7 @@ import hashlib
 import shutil
 import os
 import subprocess
-L1 = ['D:\\PAPER', 'D:\\BOOK2', 'C:\\Users\\65132\\Documents\\WeChat Files\\wxid_4ri4cmu08nhl22\\FileStorage\\File\\2020-09']
+L1 = ['D:\\PAPER', 'D:\\BOOK2', 'C:\\Users\\65132\\Documents\\WeChat Files\\wxid_4ri4cmu08nhl22\\FileStorage\\File\\2020-10']
 L2 = 'D:\ODrive\OneDrive - The Hong Kong Polytechnic University\\'
 
 def pl(L, L_new):
